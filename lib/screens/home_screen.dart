70203afd74073014bc802a0fefbe2f37f68b6bac
Sylva <sylvaintagnabou@gmail.com>
2022-08-19 16:56:09 +0000

    ...

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -8,27 +8,43 @@ import 'package:tchat/pages/contacts_page.dart';
 import 'package:tchat/pages/messages_page.dart';
 import 'package:tchat/pages/notifications_page.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
   final pages = const [
     MessagesPage(),
     NotificationPage(),
     CallPage(),
     ContactPage(),
   ];
+  var index =0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: pages[0],
-      bottomNavigationBar: _BottomNavigationBar(),
+      body: pages[index],
+      bottomNavigationBar: _BottomNavigationBar(
+        onItemSelected: (i){
+          setState(() {
+            index = i;
+          });
+        },
+      ),
     );
   }
 }
 
 class _BottomNavigationBar extends StatelessWidget{
-  const _BottomNavigationBar({Key? key}) : super(key: key);
+  const _BottomNavigationBar({Key? key,
+  required this.onItemSelected
+  }) : super(key: key);
+
+  final ValueChanged<int> onItemSelected; 
 
   @override
   Widget build(BuildContext context){
@@ -37,26 +53,30 @@ class _BottomNavigationBar extends StatelessWidget{
       bottom: true,
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: const [
+        children:  [
           _NavigationBatItem(
             index: 0,
             label: 'messages',
-            icon: CupertinoIcons.bubble_left_bubble_right_fill,
+            icon: CupertinoIcons.bubble_left_bubble_right_fill, 
+            onTapped: onItemSelected,
           ),
           _NavigationBatItem(
             index: 1,
             label: 'notifications',
             icon: CupertinoIcons.bell_solid,
+            onTapped: onItemSelected,
           ),
           _NavigationBatItem(
             index: 2,
             label: 'calls',
             icon: CupertinoIcons.phone_fill,
+            onTapped: onItemSelected,
           ),
           _NavigationBatItem(
             index: 3,
             label: 'contacts',
             icon: CupertinoIcons.person_2_fill,
+            onTapped: onItemSelected,
           ),
         ],
       ),
@@ -69,18 +89,21 @@ class _NavigationBatItem extends StatelessWidget {
     Key? key, 
     required this.index,
     required this.label,
-    required this.icon
+    required this.icon,
+    required this.onTapped
   }) : super(key: key);
+
 
   final int index;
   final String label;
   final IconData icon;
+  final ValueChanged<int> onTapped; 
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: (){
-        print(index);
+        onTapped(index);
       },
       child: SizedBox(
         height: 70,
